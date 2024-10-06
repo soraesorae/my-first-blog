891f634162a86261d0bdedeeaa7920734179a55e
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-10-06 11:53:57 +0900

    Add  for new post

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,4 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
+urlpatterns = [path('admin/', admin.site.urls), path('', include('blog.urls'))]
