ec5728d16e54f7b2806ebafa5b77f6b3e41f9333
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-04 09:45:55 +0900

    Set default timezone

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     image = models.ImageField(upload_to='blog_image/%Y/%m/%d/')
 
     def publish(self):
